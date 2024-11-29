603dc88954c90f237ca4378e0aaf3624e8c3ad20
Eric Freitas <freitaseric@proton.me>
2024-11-29 11:22:52 -0400

    cli migration

--- src/rspm.cr
+++ src/rspm.cr
@@ -1,7 +1,23 @@
-require "./cli.cr"
+require "./lib.cr"
+require "option_parser"
 
 # TODO: Write documentation for `Rspm`
 module Rspm
-  cli = CLI.new
-  cli.parse(ARGV)
+  parser = OptionParser.new do |parser|
+    parser.banner = Constants::NAME
+
+    parser.on "-v", "--version", "Show program version" do
+      puts "#{Constants::NAME} @ version #{Constants::VERSION}\nCopyright (C) Eric Freitas 2024"
+      exit
+    end
+
+    parser.on "-h", "--help", "Show help message" do
+      puts parser
+      exit
+    end
+  end
+
+  parser.parse
+
+  puts parser
 end
